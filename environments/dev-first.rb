350cc080e6b5a8a1ded94d8e2fba5d380d279227
Kyle Watson <kw@headwire.com>
2014-04-21 14:59:29 -0700

    More development trying to get a vagrant vm up and configured

---
+++ environments/dev-first.rb
@@ -0,0 +1,11 @@
+override_attributes ({
+  "dokuwiki" => {
+    "server_name" => "dokuwiki.dev",
+    "docroot" => "/home/vagrant/apps/dokuwiki"
+  },
+  "headwire" => {
+    "server_name" => "logstash.dev",
+    "docroot" => "/home/vagrant/apps/logstash"
+  }
+})
+
--- environments/dev.rb
+++
@@ -1,6 +0,0 @@
-override_attributes ({
-  "dokuwiki" => {
-    "server_name" => "dokuwiki.dev",
-    "docroot" => "/home/vagrant/apps/dokuwiki"
-  }
-})
