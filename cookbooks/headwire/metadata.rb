399584ecca109bc29ba40855a55c42f91b4c3ff9
Kyle Watson <kw@headwire.com>
2014-04-22 04:36:06 -0700

    Added kitchen stuff

--- cookbooks/headwire/metadata.rb
+++ cookbooks/headwire/metadata.rb
@@ -1,7 +1,7 @@
 name             "headwire"
-maintainer       "Example Com"
-maintainer_email "ops@example.com"
+maintainer       "headwire.com, Inc."
+maintainer_email "kw@headwire.com"
 license          "Apache 2.0"
 description      "Installs/Configures headwire"
 
-version          "0.1"
+version          "0.1.0"
