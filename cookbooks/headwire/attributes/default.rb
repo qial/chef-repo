1e1cc3091c25eb7705bc4ec8581f7c65d95a9fdb
Kyle Watson <kw@headwire.com>
2014-04-24 15:28:50 -0700

    changed some settings, began adding gitlab

--- cookbooks/headwire/attributes/default.rb
+++ cookbooks/headwire/attributes/default.rb
@@ -1,3 +1,4 @@
+
 default[:logstash][:dir]       = "/etc/logstash"
 default[:logstash][:data_dir]  = "/var/lib/logstash"
 default[:logstash][:log_dir]   = "/var/log/logstash"
@@ -7,3 +8,10 @@ default[:logstash][:user]      = "logstash"
 default[:logstash][:port]      = 6379
 default[:logstash][:bind]      = "127.0.0.1"
 
+#default[:nexus][:url] = "http://www.sonatype.org/downloads/nexus-latest-bundle.tar.gz"
+default[:nexus][:version] = "2.8.0-05"
+default[:nexus][:url] = "http://download.sonatype.com/nexus/oss/nexus-#{node[:nexus][:version]}.war"
+default[:nexus][:checksum] = "e1cece1ae5eb3a12f857e2368a3e9dbc"
+
+default[:jenkins][:url] = "http://mirrors.jenkins-ci.org/war/latest/jenkins.war"
+# default[:jenkins][:checksum] = no checksum right now
--- cookbooks/headwire/recipes/teamserver.rb
+++ cookbooks/headwire/recipes/teamserver.rb
@@ -17,15 +17,42 @@ include_recipe "apache2"
 # Set up Tomcat
 include_recipe "tomcat"
 
-# Set up Jenkins
-# TODO
+# Download Jenkins
+remote_file "#{Chef::Config['file_cache_path']}/jenkins.war" do
+  source    node[:jenkins][:url]
+  mode      00644
+  not_if "test -f #{Chef::Config['file_cache_path']}/jenkins.war"
+end
 
-# Set up Nexus
-# TODO
+# Download Nexus
+remote_file "#{Chef::Config['file_cache_path']}/nexus-#{node[:nexus][:version]}.war" do
+  source    node[:nexus][:url]
+  checksum  node[:nexus][:checksum]
+  mode      00644
+  not_if "test -f #{Chef::Config['file_cache_path']}/nexus-#{node[:nexus][:version]}.war"
+end
+
+# Install Nexus to Tomcat
+execute "Installing Nexus #{node[:nexus][:version]} from WAR" do
+  cwd Chef::Config['file_cache_path']
+  command "(mv nexus-#{node[:nexus][:version]}.war /var/lib/tomcat6/webapps/nexus.war)"
+# <<-COMMAND  COMMAND
+#    (mkdir git-#{node['git']['version']} && tar -zxf git-#{node['git']['version']}.tar.gz -C git-#{node['git']['version']} --strip-components 1)
+#    (cd git-#{node['git']['version']} && make prefix=#{node['git']['prefix']} install)
+#  creates "#{node['git']['prefix']}/bin/git"
+#  not_if "git --version | grep #{node['git']['version']}"
+end
+
+# Install Jenkins to Tomcat
+execute "Installing Jenkins from WAR" do
+  cwd Chef::Config['file_cache_path']
+  command "(mv jenkins.war /var/lib/tomcat6/webapps/jenkins.war)"
+end
 
 # Set up GitLab
 #getting a strange error about not finding a "ruby" recipe in mysql
-#include_recipe "gitlab"
+include_recipe "gitlab::setup"
+include_recipe "gitlab::deploy"
 
 #echo "OHAAAAIIIIIII"
 
